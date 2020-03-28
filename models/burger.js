4eecbf4e7018195195d4c023fe0d2cc597508223
Jessie Taylor <jtaylor@jumpcrew.com>
2020-03-27 22:43:35 -0500

    worked on updateOne. Made improvements but not successful yet

--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,23 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-selectAll: function(cbController){
-       orm.selectAll("burgers", function(data){
-             cbController(data)
-       })
-},
-insertOne: function(newBurger,cbController){
-   orm.insertOne("burgers", ["burger_name", "devoured"], [newBurger, false], function(data){
-       cbController(data)
-   })
-}
+
+    selectAll: function (cbController) {
+        orm.selectAll("burgers", function (data) {
+            cbController(data)
+        })
+    },
+    insertOne: function (newBurger, cbController) {
+        orm.insertOne("burgers", ["burger_name", "devoured"], [newBurger, false], function (data) {
+            cbController(data)
+        })
+    },
+    // test code for updateOne from Jessie
+    updateOne: function (cbController) {
+        orm.updateOne("burgers", ["devoured"], [true], function (res) {
+            cbController(res);
+        });
+    }
 
 }
 
