522dca5200db98038f2c675c2fcbcb0e02641ffe
Jessie Taylor <jtaylor@jumpcrew.com>
2020-03-31 17:29:08 -0500

    pushed to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 var mysql = require("mysql");
 require("dotenv").config()
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: "burgers_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: "burgers_db"
+  });
+  
+}
+
 
 
 connection.connect(function (err) {
